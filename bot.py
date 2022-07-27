716e9f2fd11ac55d6bbc27f36d9000946aa5c247
Denys G. Santos <gsdenys@gmail.com>
2022-07-27 16:27:02 -0400

    removing pooling

--- bot.py
+++ bot.py
@@ -49,12 +49,12 @@ def main() -> None:
     dispatcher.add_handler(list_handler)
 
     # Start the Bot
-    updater.start_polling()
+    # updater.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    # updater.idle()
 
 
      # Start the Bot
